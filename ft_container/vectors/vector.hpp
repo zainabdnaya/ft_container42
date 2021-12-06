0be59e9e3ae615ed8f6ef71a0cc1932f220252e1
zdnaya <zdnaya@student.1337.ma>
2021-12-06 14:46:55 +0100

    feat:init vector.hpp

--- ft_container/vectors/vector.hpp
+++ ft_container/vectors/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 21:08:01 by zainabdnaya       #+#    #+#             */
-/*   Updated: 2021/12/02 20:00:26 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/12/06 14:28:13 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,54 @@
 
 namespace ft 
 {
-    template <typename T>
+    template <typename T,class Alloc = std::allocator<T> >
     class vector
     {
         public:
             typedef T value_type;
+            typedef Alloc<T> allocator_type;
+            typedef Alloc<T&> reference;
+            typedef Alloc<const T&> const_reference;
+            typedef Alloc<T*> pointer;
+            typedef Alloc<const T*> const_pointer;
             typedef T* iterator;
             typedef const T* const_iterator;
             typedef ft::reverse_iterator<iterator> reverse_iterator;
             typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
-            typedef size_t size_type;
             typedef ptrdiff_t difference_type;
-            typedef T& reference;
-            typedef const T& const_reference;
+            typedef size_t size_type;
+        
 
-    //         vector();
+            explicit vector(const Alloc& alloc = Alloc()));
+            explicit vector(size_type n, const T& value, const Alloc& alloc = Alloc())
+            {
+                _alloc = alloc;
+                _size = n;
+                _capacity = n;
+                _data = _alloc.allocate(_capacity);
+                for (size_type i = 0; i < _size; i++)
+                    _alloc.construct(&_data[i], value);
+            };
+            template <class InputIterator>
+            vector(InputIterator first, InputIterator last, const Alloc& alloc = Alloc())
+            {
+                _alloc = alloc;
+                _size = 0;
+                _capacity = 0;
+                _data = nullptr;
+                for (; first != last; first++)
+                    push_back(*first);
+            };
+            vector(const vector& other)
+            {
+                _alloc = other._alloc;
+                _size = other._size;
+                _capacity = other._capacity;
+                _data = _alloc.allocate(_capacity);
+                for (size_type i = 0; i < _size; i++)
+                    _alloc.construct(&_data[i], other._data[i]);
+            };
+            
     //         vector(size_type n);
     //         vector(size_type n, const T& val);
     //         vector(const vector& other);
@@ -79,10 +112,12 @@ namespace ft
 
     //         void swap(vector& other);
 
-    //     private:
-    //         T* _data;
-    //         size_type _size;
-    //         size_type _capacity;
+        private:
+            T* _data;
+            size_type _size;
+            size_type _capacity;
+            allocator_type _alloc;
+            
     };
 
     // template <typename T>
