bbbdd770c9fae9ec4986d0b8d4ed5d58fc7bb338
zdnaya <zdnaya@student.1337.ma>
2021-12-14 15:47:11 +0100

    loading;still working on insert and reb black tree algorithm

--- ft_container/map/map.hpp
+++ ft_container/map/map.hpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 00:06:02 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/12/12 15:51:50 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/12/14 15:41:02 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,30 +24,96 @@ namespace ft
               class Alloc = std::allocator<ft::pair<const Key, T> > >
     class map
     {
-        public:
-            typedef Key                                                 key_type;
-            typedef T                                                   data_type;
-            typedef std::pair<const Key, T>                            value_type;
-            typedef Compare                                             key_compare;
-            typedef Alloc                                               allocator_type;
-            typedef typename allocator_type::reference                  reference;
-            typedef typename allocator_type::const_reference            const_reference;
-            typedef typename allocator_type::pointer                    pointer;
-            typedef typename allocator_type::const_pointer              const_pointer;   
-            typedef typename allocator_type::size_type                  size_type;
-            typedef typename allocator_type::difference_type            difference_type;
-            typedef typename allocator_type::value_type                 value_type;
-            typedef typename allocator_type::pointer                    iterator;
-            typedef typename allocator_type::const_pointer              const_iterator;
-            typedef typename allocator_type::pointer                    reverse_iterator;
-            typedef typename allocator_type::const_pointer              const_reverse_iterator;
-            typedef typename allocator_type::pointer                    pointer;
-            typedef typename size_type                                  size_type;
-            
+    public:
+        typedef Key key_type;
+        typedef T data_type;
+        typedef pair<const Key, T> value_type;
+        
+        typedef Compare key_compare;
+        typedef Alloc allocator_type;
+        
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
 
-            // constructors
+
+        typedef typename ft::iterator_traits::iterator_category iterator;
+        typedef typename ft::iterator_traits::iterator_category const_iterator;
+
+        typedef typename ft::reverse_iterator<iterator> reverse_iterator;
+        typedef typename ft::reverse_iterator<const_iterator> const_reverse_iterator;
+        typename ft::iterator_traits<iterator>::difference_type difference_type;
+        typedef typename size_type size_type;
+
+        class valaue_compare
+        {
+        protected:
+            Compare comp;
+            value_compare(Compare c) : comp(c) {}
+
+        public:
+            typedef bool result_type;
+            typedef value_type first_argument_type;
+            typedef value_type second_argument_type;
+            bool operator()(const value_type &x, const value_type &y) const
+            {
+                return comp(x.first, y.first);
+            }
+        };
+
+        // constructors
+        // Constructs an empty container, with no elements.
+        explicit map(const Compare &comp = Compare(), const Alloc &alloc = Alloc());
+        // range constructor
+        // Constructs a container with as many elements as the range [first,last), with each element constructed from its corresponding element in that range.
+        template <class InputIterator>
+        map(InputIterator first, InputIterator last, const Compare &comp = Compare(), const Alloc &alloc = Alloc())
+        {
+            _comp = comp;
+            _alloc = alloc;
+            for (; first != last; ++first)
+                insert(*first);
+        }
+        // copy constructor
+        map(const map &other);
+        {
+            _comp = other._comp;
+            _alloc = other._alloc;
+            _map = other._map;
+            _size = other._size;
+            _key = other._key;
+            _data = other._data;
+        }
+        // destructor
+        ~map()
+        {
+            for (auto it = _map.begin(); it != _map.end(); ++it)
+                _alloc.destroy(&*it);
+            _alloc.deallocate(_map.begin(), _map.size());
+        }
+        // assignment operator
+        map &operator=(const map &other);
+        {
+            _comp = other._comp;
+            _alloc = other._alloc;
+            _map = other._map;
+        }
+        // iterators
+        // begin: Returns an iterator referring to the first element in the map container.
+        iterator begin()
+        {
             
-            
+        }
+
+    private:
+        
+        const Compare &_comp;
+        const Alloc &_alloc;
+        key_type _key;
+        data_type _data;
+        size_type _size;
+        
     };
 }
 
--- practice/black-red.cpp
+++ practice/black-red.cpp
@@ -6,18 +6,18 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 20:28:21 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/12/13 22:30:45 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/12/14 15:37:02 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <map>
 
-template <typename T>
+template <typename T,typename U> 
 struct TreeNode
 {
-    T val;
     T key;
+    U val;
     TreeNode *left;
     TreeNode *right;
     TreeNode *root;
@@ -32,54 +32,31 @@ struct TreeNode
     }
 };
 
+// template <typename T,typename U> 
+//insert 
 
-void  insert(TreeNode<int> *&root, int key, int val)
-{
-    if (root == NULL)
-    {
-        root = new TreeNode<int>(key, val);
-        root->isBlack = false;
-        return ;
-    }
-    if (key < root->key)
-        insert(root->left, key, val);
-    else if (key > root->key)
-        insert(root->right, key, val);
-    else
-        root->val = val;
-    if (root == NULL)
-    {
-        TreeNode<int> *node = new TreeNode<int>(key, val);
-    }
-}
 
-void print_tree(TreeNode<int> *root)
-{
-    if (root == NULL)
-        return ;
-    std::cout << "for i : "  << root->key << "==> " << root->val << std::endl;
-    print_tree(root->left);
-    print_tree(root->right);
-}
 int main()
 {
     // map
     std::map<int, int> map;
-    map.insert(std::pair<int, int>(0, 0));
+    map.insert(std::pair<int, int>(1, 0));
     map.insert(std::pair<int, int>(1, 1));
-    map.insert(std::pair<int, int>(2, 2));
-    map.insert(std::pair<int, int>(3, 3));
+    map.insert(std::pair<int, int>(20,-20));
+    map.insert(std::pair<int, int>(0, 1));
+    map.insert(std::pair<int, int>(5, 2));
     for (int i = 0; i < map.size(); i++)
     {
+        // std::cout << "Map \n ";
         std::cout << "for i: " << i << " ==> " << map[i] << std::endl;
     }
     std::cout << "===========================" << std::endl;
-    // tree
-    TreeNode<int> *root = NULL;
-    insert(root, 0, 0);
-    insert(root, 1, 1);
-    insert(root, 2, 2);
-    insert(root, 3, 3);
-    print_tree(root);
+    // // tree
+    // TreeNode<int> *root = NULL;
+    // insert(root, 0, 0);
+    // insert(root, 1, 1);
+    // insert(root, 2, 2);
+    // insert(root, 3, 3);
+    // print_tree(root);
     return 0;
 }
