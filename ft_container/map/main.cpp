b52d4fa6000f4ecd08d5bd172d99590c714ebd83
zdnaya <zdnaya@student.1337.ma>
2021-12-19 01:14:39 +0100

    building: strp ny step into balancig

--- ft_container/map/main.cpp
+++ ft_container/map/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 19:47:34 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/12/18 05:02:44 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/12/19 01:13:55 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ public:
         int val;
         unsigned int height;
         bool isBlack;
-        // bool isLeft;
+        bool isLeft;
         // bool isRight;
         node *left;
         node *right;
@@ -36,8 +36,8 @@ public:
             left = NULL;
             right = NULL;
             isBlack = false;
-            this->key =key;
-            // isLeft = false;
+            this->key = key;
+            isLeft = false;
             // isRight = false;
             height = 1;
         }
@@ -45,7 +45,7 @@ public:
     } node;
     node *fill_node(int key, int value)
     {
-         node *_node = new node(key);
+        node *_node = new node(key);
         _node->val = value;
         return (_node);
     }
@@ -53,13 +53,13 @@ public:
     ~RBT();
     node *root;
     void print_tree(node *_root);
-    void insert_node( node *_new);
+    void insert_node(node *_new);
     // void color_fix(RBT *node);
     // void rotate_left(node *node);
     // void rotate_right(node *node);
     // void rotae_left_right(node *node);
     // void rotae_right_left(node *node);
-    // void check_balance(RBT *node);
+    void check_balance(node *node);
 };
 
 RBT::RBT()
@@ -73,15 +73,13 @@ RBT::~RBT()
 
 void RBT::print_tree(node *_root)
 {
-        //  && !root->right )
-        //     return;
-        if(!_root)
-            return;
-        print_tree(_root->left);
-        std::cout << "---> " << _root->key << "  " << _root->val << " " << _root->parent->key << std::endl;
-        print_tree(_root->right);
-
-         
+    //  && !root->right )
+    //     return;
+    if (!_root)
+        return;
+    print_tree(_root->left);
+    std::cout << "---> " << _root->key << "  " << _root->val << " " << _root->parent->key << std::endl;
+    print_tree(_root->right);
 }
 
 void RBT::insert_node(struct node *_new)
@@ -95,8 +93,8 @@ void RBT::insert_node(struct node *_new)
     }
     else
     {
-         node *tmp = root;
-         node *tmp2 = NULL;
+        node *tmp = root;
+        node *tmp2 = NULL;
 
         while (tmp != NULL)
         {
@@ -114,19 +112,59 @@ void RBT::insert_node(struct node *_new)
         if (tmp2->key > _new->key)
         {
             tmp2->left = _new;
+            _new->isLeft = true;
         }
         else if (tmp2->key < _new->key)
         {
             tmp2->right = _new;
+            _new->isLeft = false;
         }
-        std::cout << _new->key << "----" << _new->parent->key <<"\n";
-
+        // std::cout << _new->key << "----" << _new->parent->key <<"\n";
+        _new->isBlack = false;
+        _new->left = NULL;
+        _new->right = NULL;
+        check_balance(_new);
     }
-    
-    // if(root->parent->parent->left == root->parent)
+}
+
+void RBT::check_balance(node *node)
+{
+    // if (node->isBlack == false && node->parent->isBlack == false && node->parent->isLeft == true)
     // {
-    //     std::cout<< "fuck u " << std::endl;
+    //     if (node->isLeft == true)
+    //         std::cout << "here I m in Left Left  \t" << node->key << " " << node->parent->key << "\n";
+    //     else
+    //         std::cout << "here I m in Left Right \t" << node->key << " " << node->parent->key << "\n";
     // }
+    // else if (node->isBlack == false && node->parent->isBlack == false && node->parent->isLeft == false)
+    // {
+    //    if (node->isLeft == true)
+    //         std::cout << "here I m in Right Left \t" << node->key << " " << node->parent->key << "\n";
+    //     else
+    //         std::cout << "here I m in Right Right \t" << node->key << " " << node->parent->key << "\n";
+    // }
+    if (node->isBlack == false && node->parent->isBlack == false && node->parent->isLeft == true)
+    {
+        if (node->isLeft == true)
+        {
+        }
+        else
+        {
+        }
+    }
+    else if (node->isBlack == false && node->parent->isBlack == false && node->parent->isLeft == false)
+    {
+        if (node->isLeft == true)
+        {
+        }
+        else
+        {
+            if (node->parent->parent->left != NULL)
+                std::cout << node->parent->parent->left->key << "\n";
+            if (node->parent->parent->right != NULL)
+                std::cout << node->parent->parent->left->key << "\n";
+        }
+    }
 }
 
 int main()
@@ -137,9 +175,7 @@ int main()
     tst.insert_node(tst.fill_node(1, 1));
     tst.insert_node(tst.fill_node(3, 3));
     tst.insert_node(tst.fill_node(4, 4));
-    tst.insert_node(tst.fill_node(5, 5));
-
-
+    // tst.insert_node(tst.fill_node(5, 5));
 
     return 0;
 }
